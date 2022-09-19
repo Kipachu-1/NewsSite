b527131f38d24c961e0cc1c18e15e86554c927dc
Kipachu <103853361+Kipachu-1@users.noreply.github.com>
2022-09-19 19:05:33 +0600

    deploy part

--- app/settings.py
+++ app/settings.py
@@ -141,8 +141,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
