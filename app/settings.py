a8b64d585012d944c3dc35562144bd6b942629d7
Kipachu <103853361+Kipachu-1@users.noreply.github.com>
2022-09-19 16:51:37 +0600

    deploy last hope

--- app/settings.py
+++ app/settings.py
@@ -141,8 +141,12 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
